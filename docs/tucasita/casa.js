e4066a31a39b43409d94655a11a5e107a94ec683
eric-ityopas <gutierrezmatajn@ipas.edu.ar>
2025-10-29 20:22:04 -0300

    uhhhhh cambie eso yyyy eso yyy tmb eso,ya

--- docs/tucasita/casa.js
+++ docs/tucasita/casa.js
@@ -6,3 +6,22 @@ function despertar() {
 
 }
 
+function mostrarIntf() {
+    document.getElementById("int-de-notas").style.visibility = "visible"
+}
+
+function cerrarInterfaz() {
+    document.getElementById("int-de-notas").style.visibility = "hidden"
+
+}
+
+function guardarNota() {
+    var nota = document.getElementById("nota-text").value
+    localStorage.setItem("nota", nota)
+
+}
+
+window.onload = function () {
+    var ultimanota = localStorage.getItem("nota")
+    document.getElementById("ult-nota-text").innerText = ultimanota
+}
